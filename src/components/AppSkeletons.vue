e3323b410a4a81da8c481214ef3297b4b3fb8561
graphieros <alec.lloyd.probert@gmail.com>
2023-10-05 08:04:48 +0200

    update home page

--- src/components/AppSkeletons.vue
+++ src/components/AppSkeletons.vue
@@ -1,7 +1,107 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import { useMainStore } from "../stores";
+
+const store = useMainStore();
+const isDarkMode = computed(() => store.isDarkMode);
 
 const skeletonConfig = ref({
+  type: "line",
+  style: {
+    backgroundColor: "#F3F4F6",
+    color: "#CCCCCC",
+    maxHeight: 500,
+    animated: false,
+    line: {
+      axis: {
+        show: true,
+        color: "#CCCCCC",
+        strokeWidth: 0.5
+      },
+      path: {
+        color: "#cccccc",
+        strokeWidth: 1,
+        showPlots: true
+      }
+    },
+    bar: {
+      axis: {
+        show: true,
+        color: "#cccccc",
+        strokeWidth: 0.5
+      },
+      borderRadius: 0.5,
+      color: "#cccccc",
+      barWidth: 9
+    },
+    chestnut: {
+      color: "#cccccc",
+    },
+    donut: {
+      color: "#cccccc",
+      strokeWidth: 64
+    },
+    onion: {
+      color: "#cccccc"
+    },
+    gauge: {
+      color: "#cccccc"
+    },
+    quadrant: {
+      grid: {
+        color: "#cccccc",
+        strokeWidth: 0.5
+      },
+      plots: {
+        radius: 1.5,
+        color: "#cccccc"
+      }
+    },
+    radar: {
+      grid: {
+        color: "#cccccc",
+        strokeWidth: 0.5
+      },
+      shapes: {
+        color: "#cccccc"
+      }
+    },
+    waffle: {
+      color: "#cccccc"
+    },
+    table: {
+      th: {
+        color: "#cccccc"
+      },
+      td: {
+        color: "#cccccc",
+        strokeWidth: 0.5
+      }
+    },
+    rating: {
+      color: "#cccccc",
+      filled: true,
+      strokeWidth: 1,
+      maxWidth: 200
+    },
+    verticalBar: {
+      axis: {
+        show: true,
+        color: "#cccccc",
+        strokeWidth: 0.5
+      },
+      borderRadius: 0.5,
+      color: "#cccccc"
+    },
+    heatmap: {
+      color: "#cccccc"
+    },
+    pyramid: {
+      color: "#cccccc"
+    }
+  }
+});
+const skeletonConfigDarkMode = ref({
   type: "line",
   style: {
     backgroundColor: "#1A1A1A",
@@ -88,6 +188,12 @@ const skeletonConfig = ref({
       },
       borderRadius: 0.5,
       color: "#5c5c5c"
+    },
+    heatmap: {
+      color: "#5c5c5c"
+    },
+    pyramid: {
+      color: "#5c5c5c"
     }
   }
 });
@@ -105,31 +211,40 @@ const skeletons = [
     {...skeletonConfig.value, type: 'bar', link: "/docs#vue-ui-xy"},
     {...skeletonConfig.value, type: 'rating', link: "/docs#vue-ui-rating"},
     {...skeletonConfig.value, type: 'chestnut', link: "/docs#vue-ui-chestnut"},
+    {},
+    {...skeletonConfig.value, type: 'heatmap', link: "/docs#vue-ui-chestnut"},
+    {...skeletonConfig.value, type: 'pyramid', link: "/docs#vue-ui-chestnut"},
+]
+
+const skeletonsDM = [
+    {...skeletonConfigDarkMode.value, type: 'line', link: "/docs#vue-ui-xy"},
+    {...skeletonConfigDarkMode.value, type: 'quadrant', link: "/docs#vue-ui-quadrant"},
+    {...skeletonConfigDarkMode.value, type: 'donut', link: "/docs#vue-ui-donut"},
+    {...skeletonConfigDarkMode.value, type: 'verticalBar', link: "/docs#vue-ui-vertical-bar"},
+    {...skeletonConfigDarkMode.value, type: 'radar', link: "/docs#vue-ui-radar"},
+    {...skeletonConfigDarkMode.value, type: 'table', link: "/docs#vue-ui-table"},
+    {...skeletonConfigDarkMode.value, type: 'gauge', link: "/docs#vue-ui-gauge"},
+    {...skeletonConfigDarkMode.value, type: 'waffle', link: "/docs#vue-ui-waffle"},
+    {...skeletonConfigDarkMode.value, type: 'onion', link: "/docs#vue-ui-onion"},
+    {...skeletonConfigDarkMode.value, type: 'bar', link: "/docs#vue-ui-xy"},
+    {...skeletonConfigDarkMode.value, type: 'rating', link: "/docs#vue-ui-rating"},
+    {...skeletonConfigDarkMode.value, type: 'chestnut', link: "/docs#vue-ui-chestnut"},
+    {},
+    {...skeletonConfigDarkMode.value, type: 'heatmap', link: "/docs#vue-ui-chestnut"},
+    {...skeletonConfigDarkMode.value, type: 'pyramid', link: "/docs#vue-ui-chestnut"},
 ]
 
 </script>
 
 <template>
-<div class="w-full grid grid-cols-4 gap-1 sm:gap-6 mt-12 mx-auto max-w-[300px] sm:max-w-[400px] mb-12">
+<div class="w-full grid grid-cols-4 sm:grid-cols-5 gap-1 sm:gap-6" v-if="isDarkMode">
+    <div v-for="skeleton in skeletonsDM" :class="`max-w-1/3 skeleton ${skeleton.type === 'radar' ? 'radar' : ''}`">
+            <VueUiSkeleton v-if="skeleton.type" :config="skeleton" />
+    </div>
+</div>
+<div class="w-full grid grid-cols-4 sm:grid-cols-5 gap-1 sm:gap-6" v-else>
     <div v-for="skeleton in skeletons" :class="`max-w-1/3 skeleton ${skeleton.type === 'radar' ? 'radar' : ''}`">
-        <router-link :to="skeleton.link">
-            <VueUiSkeleton :config="skeleton" />
-        </router-link>
+            <VueUiSkeleton v-if="skeleton.type" :config="skeleton" />
     </div>
 </div>
 </template>
-
-<style>
-
-.skeleton {
-    opacity: 0.6;
-    transform: scale(0.9,0.9);
-    transition: opacity 0.1s ease-in-out, transform 0.1s ease-in-out;
-}
-
-.skeleton:hover {
-    opacity: 1;
-    transform: scale(1,1);
-}
-
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@ import { BrandGithubFilledIcon } from "vue-tabler-icons";
 import { useMainStore } from "../stores";
 import { BrightnessUpIcon, MoonIcon } from "vue-tabler-icons";
 import Logo from "../components/docs/Logo.vue";
+import AppSkeletons from "../components/AppSkeletons.vue";
 
 const store = useMainStore();
 
@@ -22,20 +23,26 @@ function changeTheme() {
 
 const isDarkMode = computed(() => store.isDarkMode);
 
+
+
 </script>
 
 <template>
     <div class="mx-auto w-5/6">
-    <div class="mx-auto flex flex-col gap-6 w-full h-[calc(100svh_-_49px)] place-items-center place-content-center">
-    <div class="relative">
-      <img src="../assets/logo.png" alt="vue data ui logo" class="h-[200px] mx-auto">
+      <div class="fixed top-0 left-0 h-screen w-screen z-0" style="opacity:0.4">
+        <div class="absolute top-0 left-0 w-full h-full" :style="isDarkMode ? 'background:radial-gradient(#5f8bee, transparent) !important' : 'background:radial-gradient(#F3F4F6, transparent)'"/>
+        <AppSkeletons/>
+      </div>
+    <div class="z-10 mx-auto flex flex-col gap-6 w-full h-[calc(100svh_-_49px)] place-items-center place-content-center">
+    <div class="relative z-10">
+      <img src="../assets/logo.png" alt="vue data ui logo" class="h-[200px] mx-auto drop-shadow-2xl">
       <!-- <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full">
         <Logo/>
       </div> -->
     </div>
-      <h1 class="mx-auto text-[48px] md:text-[64px] font-satoshi-bold"><span class="text-app-green">Vue</span> <span class="text-app-blue">Data UI</span></h1>
-      <p class="mx-auto text-xl text-black dark:text-gray-500 text-center">A user-empowering data visualization Vue components library</p>
-      <div class="flex flex-row gap-6 mt-6">
+      <h1 class="z-10 mx-auto text-[48px] md:text-[64px] font-satoshi-bold"><span class="text-app-green">Vue</span> <span class="text-app-blue">Data UI</span></h1>
+      <p class="z-10 mx-auto text-xl text-black dark:text-gray-200 text-center">A user-empowering data visualization Vue components library</p>
+      <div class="z-10 flex flex-row gap-6 mt-6">
         <router-link to="/installation">
             <button class="bg-gradient-to-br from-app-green to-app-blue py-3 px-5 rounded-md text-white dark:text-black font-satoshi-bold hover:shadow-xl hover:from-app-blue hover:to-app-green transition-all">Installation</button>
         </router-link>
@@ -43,13 +50,13 @@ const isDarkMode = computed(() => store.isDarkMode);
             <button class="bg-white hover:shadow-xl dark:bg-black from-app-green to-app-blue py-3 px-5 rounded-md text-black dark:text-gray-400 border border-gray-400 font-satoshi-bold dark:hover:bg-[rgba(255,255,255,0.05)] transition-all hover:border-app-blue">Docs</button>
         </router-link>
       </div>
-      <a href="https://github.com/graphieros/vue-data-ui" target="_blank">
-          <button class="flex flex-row place-content-center place-items-center bg-white dark:bg-black from-app-green to-app-blue py-3 px-5 rounded-md text-black dark:text-gray-400 border border-gray-400 font-satoshi-bold hover:shadow-xl  dark:hover:bg-[rgba(255,255,255,0.05)] hover:border-app-blue w-[220px] gap-3 transition-all">
+      <a href="https://github.com/graphieros/vue-data-ui" target="_blank" class="z-10">
+          <button class="flex flex-row place-content-center place-items-center bg-white dark:bg-black from-app-green to-app-blue py-3 px-5 rounded-md text-black dark:text-gray-400 border border-gray-400 font-satoshi-bold hover:shadow-xl  dark:hover:bg-[rgba(255,255,255,0.02)] hover:border-app-blue w-[220px] gap-3 transition-all">
             <BrandGithubFilledIcon/>
             <span>GitHub repo</span>
           </button>
         </a>
-        <button @click="changeTheme" class="dark:text-gray-400 hover:underline flex flex-row gap-2 place-items-center">
+        <button @click="changeTheme" class="z-10 dark:text-gray-400 hover:underline flex flex-row gap-2 place-items-center">
           <BrightnessUpIcon v-if="store.isDarkMode"/>
                     <MoonIcon v-else/>{{ isDarkMode ? 'Switch to light mode' : 'Switch to dark mode' }}
         </button>
