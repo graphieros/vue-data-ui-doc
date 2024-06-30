abb51ae6c84529d8d4c0cf17f7f093e3c4e26067
graphieros <alec.lloyd.probert@gmail.com>
2024-06-30 09:26:45 +0200

    update

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,7 +4,7 @@
 <template>
     <div class="w-full flex flex-col place-items-center justify-center h-screen gap-6">
         <h1 class="text-app-blue text-[64px] font-black">404</h1>
-        <p>The page you are looking for does not appear to exist.</p>
+        <p>The page you are looking for does not appear to exist...</p>
         <router-link to="/">
             <button class="bg-gradient-to-br from-app-green to-app-blue py-3 px-5 rounded-md text-black font-bold hover:from-app-blue hover:to-app-green transition-colors">Home page</button>
         </router-link>
