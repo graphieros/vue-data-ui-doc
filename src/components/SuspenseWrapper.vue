9833876596a2ed85d287881ebde318043f7528f7
graphieros <alec.lloyd.probert@gmail.com>
2024-09-30 06:36:23 +0200

    Add suspense wrapper with aspect ratio

---
+++ src/components/SuspenseWrapper.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, computed } from "vue";
+
+const props = defineProps({
+    height: {
+        type: Number,
+        default: 1
+    },
+    width: {
+        type: Number,
+        default: 1
+    },
+    background: {
+        type: String,
+        default: 'inherit'
+    }
+});
+    
+</script>
+
+<template>
+    <Suspense>
+        <template #default>
+            <slot/>
+        </template>
+        <template #fallback>
+            <div :style="{ background: background, width: '100%', aspectRatio: `${width} / ${height}` }"/>
+        </template>
+    </Suspense>
+</template>
