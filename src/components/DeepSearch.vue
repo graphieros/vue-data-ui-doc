7af4c175c2919ae440ccdba36df55fe3d909f45d
graphieros <alec.lloyd.probert@gmail.com>
2024-10-06 16:51:06 +0200

    Remove dead code

--- src/components/DeepSearch.vue
+++ src/components/DeepSearch.vue
@@ -82,54 +82,6 @@ function search() {
   }
 }
 
-// function search() {
-//   const results = [];
-//   objNames.value = [];
-
-//   function searchObject(obj, objName = "", path = "") {
-//     for (let key in obj) {
-//       if (obj.hasOwnProperty(key)) {
-//         const newPath = path ? `${path} ➜ ${key}` : key;
-//         const fullPath = objName ? `${formatString(objName)} : ${newPath}` : newPath;
-
-//         if (key === searchTerm.value) {
-//           results.push({
-//             path: fullPath,
-//             value: obj[key],
-//             type: typeof obj[key],
-//             route: objName.replaceAll("_", "-"),
-//           });
-//           if (objName && !objNames.value.includes(formatString(objName))) {
-//             objNames.value.push(formatString(objName));
-//           }
-//         }
-
-//         if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
-//           searchObject(obj[key], objName || key, newPath);
-//         }
-//       }
-//     }
-//   }
-//   Object.keys(config).forEach((key) => {
-//     searchObject(config[key], key);
-//   });
-//   hasResults.value = results.length > 0;
-//   currentResults.value = results.filter((r) => {
-//     if (!selectedObjName.value) return r;
-//     return r.path.toUpperCase().includes(selectedObjName.value.toUpperCase());
-//   });
-
-//   useModal("open");
-//   return results;
-// }
-
-function formatString(str) {
-  return str
-    .split("_")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join("");
-}
-
 function useModal(state) {
   const dialog = document.getElementById("searchDialog");
   isClosing.value = false;
