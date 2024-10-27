de99aa5d5a2403a1ac7eae5f9ef13b87e387e888
graphieros <alec.lloyd.probert@gmail.com>
2024-10-27 10:52:23 +0100

    Improve doc page tabs menu design

--- src/components/BaseTabsMenu.vue
+++ src/components/BaseTabsMenu.vue
@@ -78,8 +78,8 @@ const selectTab = (item) => {
         </button>
 
         <div ref="tabsContainer" class="flex select-none overflow-x-auto space-x-4 scrollbar-hide w-full px-8" @scroll="onScroll">
-            <div v-for="(item, index) in items" :key="index" :style="{ color: item.color, background: activeTab === item.order ? `${item.color}20` : 'transparent', borderBottom: activeTab === item.order ? `2px solid ${item.color}` : '2px solid transparent' }"
-                :class="`flex flex-col items-center text-center justify-center cursor-pointer p-2 min-w-[90px] flex-shrink-0 whitespace-nowrap  rounded-tl-md rounded-tr-md gap-2`" @click="selectTab(item)">
+            <div v-for="(item, index) in items" :key="index" :style="{ color: item.color, background: activeTab === item.order ? `${item.color}20` : undefined, borderBottom: activeTab === item.order ? `2px solid ${item.color}` : '2px solid transparent' }"
+                :class="`flex flex-col items-center text-center justify-center cursor-pointer p-2 min-w-[90px] flex-shrink-0 whitespace-nowrap rounded-tl-md rounded-tr-md gap-2 hover:bg-[#1A1A1A10] dark:hover:bg-[#FFFFFF05] transition-colors`" @click="selectTab(item)">
                 <IconSettings v-if="item.icon === 'settings'" :stroke="isDarkMode ? '#5F8BEE' : '#1A1A1A'" :size="26" />
                 <VueUiIcon v-else :name="item.icon" :stroke="item.color" />
                 <span class="text-sm" dir="auto">{{ item.name }}</span>
