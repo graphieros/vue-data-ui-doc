63f6171fa0b5e1db6b20a0bb064f27a52e08deb7
graphieros <alec.lloyd.probert@gmail.com>
2024-10-30 07:46:07 +0100

    Improve examples action buttons

--- src/components/examples/components/TokoTsubo.vue
+++ src/components/examples/components/TokoTsubo.vue
@@ -9,17 +9,19 @@ defineProps(["componentName", "dataset", "config", "id", "link", "hideMaker"])
 </script>
 
 <template>
-    <BaseDocHeaderActions
-        :targetDoc="link"
-        :targetMaker="componentName"
-        :defaultConfig="false"
-    >
-        <template #grabber>
-            <Grabber v-if="dataset" :dataset="dataset" :config="config" :componentName="componentName" :id="id">
-                <template #icon>
-                    <slot name="icon"></slot>
-                </template>
-            </Grabber>
-        </template>
-    </BaseDocHeaderActions>
+    <div class="w-full dark:bg-[#FFFFFF05] bg-gray-100 rounded shadow">
+        <BaseDocHeaderActions
+            :targetDoc="link"
+            :targetMaker="componentName"
+            :defaultConfig="false"
+        >
+            <template #grabber>
+                <Grabber v-if="dataset" :dataset="dataset" :config="config" :componentName="componentName" :id="id">
+                    <template #icon>
+                        <slot name="icon"></slot>
+                    </template>
+                </Grabber>
+            </template>
+        </BaseDocHeaderActions>
+    </div>
 </template>
