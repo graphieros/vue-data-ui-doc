0c1d086e9cdf16328ef8f2196e96edd075bed2c3
graphieros <alec.lloyd.probert@gmail.com>
2024-11-10 10:37:14 +0100

    Update maker

--- src/components/maker/MakerKnobs.vue
+++ src/components/maker/MakerKnobs.vue
@@ -49,13 +49,35 @@ const warningColorAlpha = ref({
     ko: "투명 색상은 v2.3.77부터 사용할 수 있습니다",
     ar: "الألوان الشفافة قابلة للاستخدام منذ الإصدار v2.3.77"
 })
+
+const useOldColorPickers = ref(false);
+
+const useTransparencyLabel = ref({
+    en: "Use old color pickers (before v2.3.77)",
+    fr: "Utilisez les anciens sélecteurs de couleurs (avant la v2.3.77)",
+    pt: "Use os antigos seletores de cores (antes da v2.3.77)",
+    de: "Verwenden Sie die alten Farbwähler (vor v2.3.77)",
+    zh: "使用旧版颜色选择器（v2.3.77之前）",
+    jp: "古いカラーピッカーを使用する（v2.3.77以前）",
+    es: "Usa los antiguos selectores de colores (antes de la v2.3.77)",
+    ko: "구버전 색상 선택기 사용 (v2.3.77 이전)",
+    ar: "استخدم أدوات اختيار الألوان القديمة (قبل الإصدار v2.3.77)"
+})
     
 </script>
 
 <template>
-    <div class="w-full border border-app-orange p-4 rounded mt-6 flex flex-row gap-2 bg-[#ff660020] text-black dark:text-white" dir="auto">
-        <VueUiIcon name="circleExclamation" stroke="#ff6600"/>
-        {{ warningColorAlpha[store.lang] }}
+    <div class="w-full border border-app-orange p-4 rounded mt-6 bg-[#ff660020] text-black dark:text-white" dir="auto">
+        <div class="flex flex-col gap-2">
+            <div class="flex flex-row gap-2 text-app-orange">
+                <VueUiIcon name="circleExclamation" stroke="#ff6600"/>
+                {{ warningColorAlpha[store.lang] }}
+            </div>
+            <div class="flex-row" dir="auto">
+                <label for="useTrans" class="px-8">{{ useTransparencyLabel[store.lang] }}</label>
+                <input id="useTrans" type="checkbox" v-model="useOldColorPickers">
+            </div>
+        </div>
     </div>
     <div class="flex flex-col gap-2 shadow dark:shadow-md bg-[#5f8bee30] p-3 rounded my-4" v-for="category in categories">
         <div class="w-full bg-gradient-to-r from-app-blue-light dark:from-app-blue-dark to-transparent pl-3 py-2 rounded text-black dark:text-white">
@@ -63,7 +85,10 @@ const warningColorAlpha = ref({
         </div>
         <div class="flex flex-row gap-4 place-items-center flex-wrap">
             <div v-for="knob in model.filter(k => k.category === category.key)" class="flex flex-col justify-start my-2">
+
                 <label v-if="knob.type !== 'color'" class="text-xs text-black dark:text-white" dir="auto">{{ getLabel(knob.label) }}</label>
+                <label v-if="knob.type === 'color' && useOldColorPickers" class="text-xs text-black dark:text-white" dir="auto">{{ getLabel(knob.label) }}</label>
+
                 <div class="flex place-items-center justify-start h-[40px]">
                     <BaseNumberInput v-if="knob.type === 'number'" v-model:value="knob.def" :min="knob.min" :max="knob.max" :step="knob.step" @change="emit('change')"/>
                     <template v-if="knob.type === 'range'">
@@ -72,7 +97,7 @@ const warningColorAlpha = ref({
                             <input type="range" v-model="knob.def" :min="knob.min" :max="knob.max" :step="knob.step" class="accent-app-blue z-10" @change="emit('change')">
                         </div>
                     </template>
-                    <BaseColorInput v-else-if="knob.type === 'color'" :label="getLabel(knob.label)" :rgba="true" v-model:value="knob.def"/>
+                    <BaseColorInput v-else-if="knob.type === 'color' && !useOldColorPickers" :label="getLabel(knob.label)" :rgba="true" v-model:value="knob.def"/>
                     <input v-else-if="!['number', 'range', 'select'].includes(knob.type)" class="accent-app-blue" v-if="!['none', 'select'].includes(knob.type)" :type="knob.type" v-model="knob.def" @change="emit('change')">
                     <select v-else-if="knob.type === 'select'" v-model="knob.def" @change="emit('change')" class="h-[32px] px-2">
                         <option v-for="opt in knob.options">{{ opt }}</option>
