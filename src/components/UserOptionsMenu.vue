927a7f1d029a44b0606549abcc73a2c7bd379d88
graphieros <alec.lloyd.probert@gmail.com>
2024-12-03 08:03:54 +0100

    Add chart names in UserOptionsMenu docs

--- src/components/UserOptionsMenu.vue
+++ src/components/UserOptionsMenu.vue
@@ -281,8 +281,11 @@ const items = ref([
                                     delay="delay-150"
                                 >
                                     <RouterLink :to="charts[c].link">
-                                        <div class="flex place-items-center justify-center p-2 border border-gray-200 dark:border-[#FFFFFF10] hover:bg-[#5f8aee30] hover:shadow-md hover:border-app-blue dark:hover:border-app-blue transition-all">
-                                            <VueUiIcon :name="charts[c].icon" :stroke="isDarkMode ? '#CCCCCC' : '#1A1A1A'" />
+                                        <div class="relative flex place-items-center justify-center p-2 border border-gray-200 dark:border-[#FFFFFF10] hover:bg-[#5f8aee30] hover:shadow-md hover:border-app-blue dark:hover:border-app-blue transition-all pb-3">
+                                            <VueUiIcon :name="charts[c].icon" :stroke="isDarkMode ? '#CCCCCC' : '#1A1A1A'" class="-mt-1" />
+                                            <div class="absolute -bottom-1 left-1/2 -translate-x-1/2 text-[6px] pb-[2px]">
+                                                {{ charts[c].name === 'VueUiParallelCoordinatePlot' ? 'PCP' : charts[c].name.replace('VueUi', '') }}
+                                            </div>
                                         </div>
                                     </RouterLink>
                                 </FlexibleTooltip>
