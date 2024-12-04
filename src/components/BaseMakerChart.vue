7ee9404f722137979c8f5336fbc5a14676df1b49
graphieros <alec.lloyd.probert@gmail.com>
2024-12-04 07:49:56 +0100

    Improve maker

--- src/components/BaseMakerChart.vue
+++ src/components/BaseMakerChart.vue
@@ -49,7 +49,7 @@ const translations = computed(() => makerStore.translations)
         </BaseDragElement>                
         <div v-else :class="`${expandedWidth} transition-all shadow-xl rounded p-2 ${isFixed ? `fixed top-[88px] right-2 z-20 ${fixedWidth}` : `w-full mx-auto ${expandedWidth}`}`">
             <div class="flex flex-row gap-2 mb-2 w-full bg-white dark:bg-[#1A1A1A] py-2 justify-center">
-                <button @click="emit('fixChart')" class="flex align-center justify-center bg-gradient-to-br from-app-blue-light to-app-blue  p-2 rounded-full opacity-80 hover:opacity-100 transition-all">
+                <button @click="emit('fixChart')" class="hidden sm:flex align-center justify-center bg-gradient-to-br from-app-blue-light to-app-blue  p-2 rounded-full opacity-80 hover:opacity-100 transition-all">
                     <PinnedOffIcon v-if="isFixed" class="text-black"/>
                     <PinIcon v-else class="text-black"/>
                 </button>
