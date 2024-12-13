c95cadb62463ce685f851919cfb31ab10e4bad90
graphieros <alec.lloyd.probert@gmail.com>
2024-12-13 17:05:38 +0100

    Add links to issue pages

--- src/components/helpCenter/HelpCenter.vue
+++ src/components/helpCenter/HelpCenter.vue
@@ -52,6 +52,34 @@ onBeforeUnmount(() => {
         helpCenter.value.removeEventListener("click", handleOutsideClick);
     }
 });
+
+const linksTranslations = computed(() => {
+    return {
+        issueLib: {
+            en: "Report an issue with the components library",
+            fr: "Signaler un problème avec la bibliothèque de composants",
+            pt: "Relatar um problema com a biblioteca de componentes",
+            de: "Ein Problem mit der Komponentenbibliothek melden",
+            zh: "报告组件库的问题",
+            jp: "コンポーネントライブラリに関する問題を報告する",
+            es: "Reportar un problema con la biblioteca de componentes",
+            ko: "컴포넌트 라이브러리 문제 보고",
+            ar: "الإبلاغ عن مشكلة في مكتبة المكونات"
+        },
+        issueSite: {
+            en: "Report an issue about this website",
+            fr: "Signaler un problème sur ce site web",
+            pt: "Relatar um problema neste site",
+            de: "Ein Problem mit dieser Website melden",
+            zh: "报告有关此网站的问题",
+            jp: "このウェブサイトに関する問題を報告する",
+            es: "Reportar un problema con este sitio web",
+            ko: "이 웹사이트 문제 신고",
+            ar: "الإبلاغ عن مشكلة في هذا الموقع"
+        }
+    }
+})
+
 </script>
 
 <template>
@@ -72,7 +100,21 @@ onBeforeUnmount(() => {
             <HelpDataset/>
             <HelpVueDataUiComponent/>
             <HelpNuxt/>
+            <div class="w-full flex flex-col place-items-start border-t border-gray-500 mt-2 pt-4">
+                <button class="p-2" dir="auto">
+                    <a href="https://github.com/graphieros/vue-data-ui/issues" target="_blank" class="underline text-app-blue">
+                        {{ linksTranslations.issueLib[store.lang] }}
+                    </a>
+                </button>
+                <button class="p-2" dir="auto">
+                    <a href="https://github.com/graphieros/vue-data-ui-doc/issues" target="_blank" class="underline text-app-blue">
+                        {{ linksTranslations.issueSite[store.lang] }}
+                    </a>
+                </button>
+            </div>
         </div>
+
+
         <div class="w-full flex place-items-center justify-end absolute bottom-6 right-6 gap-4">
             <button
                 class="h-[40px] px-6 rounded-lg border border-app-orange text-black dark:text-app-orange hover:bg-gradient-to-br hover:from-app-orange hover:to-orange-700 hover:text-white hover:dark:text-white transition-colors"
