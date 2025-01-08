c074e32825acd6c615a5450ffcd4073775e30a77
graphieros <alec.lloyd.probert@gmail.com>
2025-01-08 07:25:02 +0100

    Add ThemeTag component

---
+++ src/components/BaseTag.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, computed } from "vue";
+
+defineProps({
+    backgroundColor: {
+        type: String,
+        default: '#e1e5e8'
+    },
+    color: {
+        type: String,
+        default: '#1A1A1A'
+    },
+    fontSize: {
+        type: String,
+        default: '1rem'
+    }
+})
+
+</script>
+
+<template>
+    <div :style="{
+        backgroundColor: backgroundColor,
+        color: color
+    }"
+        class="pl-2 pr-4 rounded-full w-fit flex flex-row gap-2 place-items-center shadow-md"
+    >
+        <slot/>
+    </div>
+</template>
---
+++ src/components/ThemeTag.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref, computed } from "vue";
+import BaseTag from "./BaseTag.vue";
+
+const props = defineProps({
+    type: {
+        type: String,
+        default: 'default'
+    }
+});
+
+const themes = ref({
+    default: {
+        backgroundColor: "#FFFFFF",
+        color: '#1A1A1A',
+        fill: '#FFFFFF'
+    },
+    zen: {
+        backgroundColor: "#fbfafa",
+        color: "#8A9892",
+        fill: "#B9B99D"
+    },
+    concrete: {
+        backgroundColor: "#f6f6fb",
+        color: "#50606C",
+        fill: "#4A6A75"
+    },
+    hack: {
+        backgroundColor: "#1A1A1A",
+        color: "#99AA99",
+        fill: "#009900"
+    }
+})
+
+</script>
+
+<template>
+    <BaseTag class="mb-4" :backgroundColor="themes[type].backgroundColor" :color="themes[type].color">
+        <svg class="shadow-md rounded-full" viewBox="0 0 10 10" height="16" width="16"><circle cx="5" cy="5" r="5" :fill="themes[type].fill"/></svg>
+        {{ type }}
+    </BaseTag>
+</template>
