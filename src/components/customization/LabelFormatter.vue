278bff5026a91538cc2ea3277aae13a79b8533b3
graphieros <alec.lloyd.probert@gmail.com>
2025-01-13 07:57:36 +0100

    Update formatter example in customization page

--- src/components/customization/LabelFormatter.vue
+++ src/components/customization/LabelFormatter.vue
@@ -11,6 +11,26 @@ const code = ref(`formatter: ({ value, config }) => {
   return \`my format:\`\${value.toLocaleString('de-DE')}\`;
 }
 `)
+
+const codeConfig = ref(`
+// Example for labels in VueUiDonut
+
+const configDonut = ref({
+  style: {
+    chart: {
+      layout: {
+        labels: {
+          value: {
+            formatter: ({ value, config }) => {
+              return \`my format:\`\${value.toLocaleString('de-DE')}\`;
+            }
+          }
+        }
+      }
+    }
+  }
+})
+`)
     
 </script>
 
@@ -20,13 +40,24 @@ const code = ref(`formatter: ({ value, config }) => {
     </div>
 
     <div
-    class="max-w-[1200px] flex flex-col lg:flex-row gap-4 justify-center place-items-center mx-auto bg-gray-200 dark:bg-[#FFFFFF10] p-4 rounded-md shadow-md"
-    style="width: calc(100% - 48px)"
-  >
-    <div
-      class="bg-black p-4 text-gray-300 text-xs w-full overflow-auto rounded-md"
+      class="max-w-[1200px] flex flex-col lg:flex-row gap-4 justify-center place-items-center mx-auto bg-gray-200 dark:bg-[#FFFFFF10] p-4 rounded-md shadow-md"
+      style="width: calc(100% - 48px)"
     >
-      <CodeParser :content="code" language="javascript"/>
-    </div>
-</div>
+      <div
+        class="bg-black p-4 text-gray-300 text-xs w-full overflow-auto rounded-md"
+      >
+        <CodeParser :content="code" language="javascript"/>
+      </div>
+  </div>
+
+    <div
+      class="max-w-[1200px] flex flex-col lg:flex-row gap-4 justify-center place-items-center mx-auto bg-gray-200 dark:bg-[#FFFFFF10] p-4 rounded-md shadow-md"
+      style="width: calc(100% - 48px)"
+    >
+      <div
+        class="bg-black p-4 text-gray-300 text-xs w-full overflow-auto rounded-md"
+      >
+        <CodeParser :content="codeConfig" language="javascript"/>
+      </div>
+  </div>
 </template>
