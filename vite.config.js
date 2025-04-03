cb7202afb1fa60ed18dd43f1d464e866a038af22
graphieros <alec.lloyd.probert@gmail.com>
2025-04-03 21:39:11 +0200

    Bump vue-data-ui from 2.6.29 to 2.6.30

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), {
+    name: "copy-llms-file",
+    closeBundle() {
+      const src = resolve(__dirname, "llms.txt");
+      const dest = resolve(__dirname, "dist", "llms.txt");
+      fs.copyFileSync(src, dest);
+      console.log("llms.txt copied to dist folder.");
+    },
+  }],
 })
