c3b196edb73988eb4f0e9c6ed116cc54b63e0fac
graphieros <alec.lloyd.probert@gmail.com>
2025-04-05 07:28:24 +0200

    Update installation page

--- src/views/Installation.vue
+++ src/views/Installation.vue
@@ -107,9 +107,9 @@ const universalComponentTemplateContent = computed(() => {
             <div class="relative w-[80px] h-[80px]">
                 <VueUiIcon name="starFill" :size="80" :strokeWidth="0.8" class="hidden md:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" stroke="#5f8bee"/>
                 <VueUiIcon name="starFill" :size="60" :strokeWidth="0.8" class="hidden md:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" :stroke="lightenHexColor({ hexColor: '#5F8BEE' , force: 0.2 })"/>
-                <VueUiIcon name="starFill" :size="40" :strokeWidth="0.8" class="hidden md:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" :stroke="lightenHexColor({ hexColor: '#5F8BEE' , force: 0.4 })"/>
-                <VueUiIcon name="starFill" :size="20" :strokeWidth="0.8" class="hidden md:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" :stroke="lightenHexColor({ hexColor: '#5F8BEE' , force: 0.6 })"/>
-                <VueUiIcon name="starFill" :size="5" :strokeWidth="0.8" class="hidden md:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" :stroke="lightenHexColor({ hexColor: '#5F8BEE' , force: 0.8 })"/>
+                <VueUiIcon name="starFill" :size="40" :strokeWidth="0.8" class="animate-ping hidden md:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" :stroke="lightenHexColor({ hexColor: '#5F8BEE' , force: 0.4 })"/>
+                <VueUiIcon name="starFill" :size="20" :strokeWidth="0.8" class="animate-ping hidden md:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" :stroke="lightenHexColor({ hexColor: '#5F8BEE' , force: 0.6 })"/>
+                <VueUiIcon name="starFill" :size="5" :strokeWidth="0.8" class="animate-ping hidden md:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" :stroke="lightenHexColor({ hexColor: '#5F8BEE' , force: 0.8 })"/>
             </div>
             <h1 class="text-[64px] sm:text-[96px] text-center">
                 {{ translations.menu.installation[store.lang] }}
