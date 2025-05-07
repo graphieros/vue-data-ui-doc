8cadd1b36164ed712107092191c91b93042d8614
graphieros <alec.lloyd.probert@gmail.com>
2025-05-07 06:22:02 +0200

    Improve icons page

--- src/components/docs/DocVueUiIcon.vue
+++ src/components/docs/DocVueUiIcon.vue
@@ -4,6 +4,8 @@ import { XIcon, CopyIcon, CheckIcon } from "vue-tabler-icons";
 import { useMainStore } from "../../stores";
 import { copyText } from "../maker/lib";
 import BaseDocTitle from "../BaseDocTitle.vue";
+import CodeParser from "../customization/CodeParser.vue";
+import ConfirmCopy from "../ConfirmCopy.vue";
 
 const store = useMainStore();
 const key = ref(0);
@@ -189,8 +191,29 @@ function useModal(state) {
     }
 }
 
+const currentIndex = ref(0);
+
+function previous() {
+    if (currentIndex.value === 0) {
+        currentIndex.value = [...icons.value, ...specialIcons.value].length - 1;
+    } else {
+        currentIndex.value -= 1;
+    }
+    selectedIcon.value = [...icons.value, ...specialIcons.value][currentIndex.value];
+}
+
+function next() {
+    if (currentIndex.value === [...icons.value,...specialIcons.value].length - 1) {
+        currentIndex.value = 0;
+    } else {
+        currentIndex.value += 1;
+    }
+    selectedIcon.value = [...icons.value, ...specialIcons.value][currentIndex.value];
+}
+
 function selectIcon(icon) {
     selectedIcon.value = icon;
+    currentIndex.value = [...icons.value,...specialIcons.value].indexOf(icon);
     useModal("open");
 }
 
@@ -211,6 +234,16 @@ function requestCopy() {
     }, 1000)
 }
 
+const code = computed(() => {
+    return `
+    <VueUiIcon
+        name="${selectedIcon.value}"
+        :size="24"
+        stroke="${ isDarkMode.value ? '#CCCCCC' : '#1A1A1A'}"
+    />
+    `
+})
+
 </script>
 
 <template>
@@ -225,7 +258,7 @@ function requestCopy() {
             <p class="mx-auto max-w-[400px] text-md text-black dark:text-gray-500 mb-2 text-center">{{ translations.docs.tooltips.iconUserOptions[store.lang] }}</p>
             <div class="flex flex-wrap gap-4 place-items-center place-content-center mt-12">
                 <div v-for="icon in icons">
-                    <div class="flex flex-col place-items-center place-content-center gap-4 w-[100px] p-6 rounded-lg cursor-pointer hover:outline hover:outline-app-blue" @click="selectIcon(icon)">
+                    <div class="flex flex-col place-items-center place-content-center gap-4 w-[100px] p-6 rounded-lg cursor-pointer hover:scale-125 hover:bg-[#00000010] dark:hover:bg-[#00000020] transition-all" @click="selectIcon(icon)">
                         <VueUiIcon :isSpin="icon === 'spin'" :name="icon" :stroke="isDarkMode ? '#FFFFFF' : '#1A1A1A'"/>
                         <div class="dark:text-gray-400 text-black text-xs">{{ icon }}</div>
                     </div>
@@ -239,37 +272,35 @@ function requestCopy() {
 
         <div class="flex flex-wrap gap-4 place-items-center place-content-center">
             <div v-for="icon in specialIcons">
-                <div class="flex flex-col place-items-center place-content-center gap-4 w-[100px] p-6 rounded-lg cursor-pointer hover:outline hover:outline-app-blue" @click="selectIcon(icon)">
+                <div class="flex flex-col place-items-center place-content-center gap-4 w-[100px] p-6 rounded-lg cursor-pointer hover:bg-[#00000010] dark:hover:bg-[#FFFFFF03] hover:scale-125 transition-all" @click="selectIcon(icon)">
                     <VueUiIcon :isSpin="icon === 'spin'" :name="icon" :stroke="isDarkMode ? '#42d392' : '#1A1A1A'"/>
                     <div class="dark:text-gray-400 text-black text-xs">{{ icon }}</div>
                 </div>
             </div>
         </div>
         
-        <dialog id="iconDialog"  :class="`glass fixed h-screen max-h-[400px] w-full max-w-[400px] p-6 rounded-lg bg-[rgba(255,255,255,0.8)] dark:bg-[rgba(36,36,36,0.8)] text-black dark:text-gray-300 border dark:border-[#2A2A2A]`">
+        <dialog id="iconDialog"  :class="`glass fixed h-screen max-h-[450px] w-full max-w-[400px] p-6 rounded-lg bg-[rgba(255,255,255,0.8)] dark:bg-[rgba(36,36,36,0.8)] text-black dark:text-gray-300 border dark:border-[#2A2A2A]`">
             <div class="z-10 w-full text-2xl sticky top-0 font-black">
-            <div class="flex flex-row gap-2 place-items-center text-app-blue">
+            <div class="select-none flex flex-row gap-2 place-items-center text-app-blue">
                 {{ selectedIcon }}
             </div>
             <button class="absolute top-2 right-2" @click="useModal('close')">
                 <XIcon class="text-black dark:text-app-green" />
             </button>
             </div>
+            
+            <button class="absolute top-1/3 left-6 -translate-y-2/3" @click="previous">
+                <VueUiIcon name="arrowLeft" stroke="#6A6A6A"/>
+            </button>
+            <button class="absolute top-1/3 right-6 -translate-y-2/3" @click="next">
+                <VueUiIcon name="arrowRight" stroke="#6A6A6A"/>
+            </button>
 
             <div class="mx-auto flex place-items-center justify-center flex-col gap-6 mt-6">
                 <VueUiIcon :name="selectedIcon" :size="128" :strokeWidth="1.2" :stroke="isDarkMode ? '#CCCCCC' : '#1A1A1A'"/>
-            <div @click="requestCopy" class="relative">
-                <CopyIcon class="absolute -top-2 -right-2 cursor-pointer" v-if="!isCopied" />
-                <CheckIcon v-else class="absolute text-app-green -top-2 -right-2 cursor-pointer animate-ping"/>
-                <code>
-                    &lt;<span class="text-app-green">VueUiIcon</span><br>
-                        &nbsp;&nbsp;&nbsp;&nbsp;name="{{ selectedIcon }}"<br>
-                        &nbsp;&nbsp;&nbsp;&nbsp;:size="24"<br>
-                        &nbsp;&nbsp;&nbsp;&nbsp;stroke="{{ isDarkMode ? '#CCCCCC' : '#1A1A1A' }}"<br>
-                    /&gt;
-                </code>
+                <CodeParser :content="code" language="html" class="select-none" @copy="store.copy()"/>
             </div>
-            </div>
+            <ConfirmCopy />
         </dialog>
 
         <div class="w-full text-center my-12">
