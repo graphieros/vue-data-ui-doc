bcb54383af749301baaeff5307b56153823fae6a
graphieros <alec.lloyd.probert@gmail.com>
2025-06-16 05:59:12 +0200

    Improve 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,12 +1,45 @@
 <script setup>
+import { computed, ref } from "vue";
+import { useMainStore } from "../stores";
+
+const store = useMainStore();
+const isDarkMode = computed(() => store.isDarkMode);
+
+const _404 = ref({
+    en: "This page cannot be found.",
+    fr: "Cette page est introuvable.",
+    pt: "Esta página não pode ser encontrada.",
+    de: "Diese Seite kann nicht gefunden werden.",
+    zh: "此页面无法找到。",
+    jp: "このページは見つかりません。",
+    es: "Esta página no se puede encontrar.",
+    ko: "이 페이지를 찾을 수 없습니다.",
+    ar: "يتعذر العثور على هذه الصفحة.",
+});
+
+const home = ref({
+    en: "Home page",
+    fr: "Page d'accueil",
+    pt: "Página inicial",
+    de: "Startseite",
+    zh: "首页",
+    jp: "ホームページ",
+    es: "Página de inicio",
+    ko: "홈페이지",
+    ar: "الصفحة الرئيسية",
+});
 </script>
 
 <template>
     <div class="w-full flex flex-col place-items-center justify-center h-screen gap-6">
-        <h1 class="text-app-blue text-[64px] font-black">404</h1>
-        <p>The page you are looking for does not appear to exist...</p>
+        <VueUiIcon name="moodNeutral" stroke="#5f8aee" :size="130" />
+        <p class="text-xl" dir="auto">{{ _404[store.lang] }}</p>
         <router-link to="/">
-            <button class="bg-gradient-to-br from-app-green to-app-blue py-3 px-5 rounded-md text-black font-bold hover:from-app-blue hover:to-app-green transition-colors">Home page</button>
+            <button
+                class="bg-gradient-to-br from-app-green to-app-blue py-3 px-5 rounded-md text-black font-bold hover:from-app-blue hover:to-app-green transition-colors flex flex-row place-items-center gap-2" dir="auto">
+                <VueUiIcon name="homeFilled" stroke="#1A1A1A" />
+                {{ home[store.lang] }}
+            </button>
         </router-link>
     </div>
 </template>
