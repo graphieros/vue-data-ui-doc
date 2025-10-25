7a695ccd43533674005a2f0e76c878bab769acd6
graphieros <alec.lloyd.probert@gmail.com>
2025-10-25 08:53:46 +0200

    Improve doc config BaseDetails

--- src/components/BaseDetails.vue
+++ src/components/BaseDetails.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted, onBeforeUnmount, computed, nextTick } from "vue";
 import { useMainStore } from "../stores";
 import useAttrMapping from "../useAttrMapping";
+import BaseCard from "./BaseCard.vue";
 
 const store = useMainStore();
 const isDarkMode = computed(() => store.isDarkMode);
@@ -110,54 +111,56 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-    <div :class="`relative mt-2 ${indent ? 'ml-6' : ''}`">
-        <details ref="det" :style="level ? `margin-left:0px` : ''"
-            :class="`border-l ${isSelected ? 'border-app-blue dark:border-app-green' : 'border-gray-500'} rounded-md pl-2 bg-[#1A1A1A05] dark:bg-[#FFFFFF05] mb-2 shadow`">
-            <summary @click="onSummaryClick" @mouseenter="isSelected = true" @mouseout="isSelected = false"
-                class="cursor-pointer -ml-2 pl-2 font-black hover:bg-[#5f8aee20] dark:hover:bg-[#42d39220] transition-colors">
-                {{ attr }}
-                <i class="text-gray-500" v-if="translatedAttr">({{ translatedAttr }})</i>
-                {{ equal ? "=" : ":" }}
-                {{ isOpen ? (brackets ? "[" : "{") : (brackets ? "[ ... ]" : "{ ... }") }}
-            </summary>
 
-            <div ref="panel" class="details-panel">
-                <div ref="inner" class="details-inner">
-                    <code class="pl-6 flex flex-col mt-1">
-                        <slot />
-                    </code>
-                    <span v-if="isOpen">{{ brackets ? "]" : "}" }}</span>
+        <div :class="`relative mt-2 ${indent ? 'ml-6' : ''}`">
+            <details ref="det" :style="level ? `margin-left:0px` : ''"
+                :class="`border-l ${isSelected ? 'border-app-blue dark:border-app-green' : 'border-gray-500'} rounded-md pl-2 bg-[#1A1A1A05] dark:bg-[#FFFFFF05] mb-2 shadow`">
+                <summary @click="onSummaryClick" @mouseenter="isSelected = true" @mouseout="isSelected = false"
+                    class="cursor-pointer -ml-2 pl-2 font-black hover:bg-[#5f8aee20] hover:dark:bg-gradient-to-r dark:hover:from-[#42d39220] dark:hover:to-transparent transition-colors">
+                    {{ attr }}
+                    <i class="text-gray-500" v-if="translatedAttr">({{ translatedAttr }})</i>
+                    {{ equal ? "=" : ":" }}
+                    {{ isOpen ? (brackets ? "[" : "{") : (brackets ? "[ ... ]" : "{ ... }") }}
+                </summary>
+    
+                <div ref="panel" class="details-panel">
+                    <div ref="inner" class="details-inner">
+                        <code class="pl-6 flex flex-col mt-1">
+                            <slot />
+                        </code>
+                        <span v-if="isOpen">{{ brackets ? "]" : "}" }}</span>
+                    </div>
                 </div>
-            </div>
-        </details>
+            </details>
+    
+            <Transition name="fade">
+                <div v-if="isSelected && title"
+                    class="text-black dark:text-gray-300 hidden sm:flex -top-10 left-0 place-items-center justify-center absolute z-10 bg-white shadow-xl dark:bg-black-100 text-xs text-left px-4 py-1 rounded-full border-2 border-black dark:border-app-green">
+                    {{ title }}
+                    <svg v-if="isOpen" class="absolute -left-4 top-1/2" viewBox="0 0 64 64" height="50" width="50" style="overflow:visible; pointer-events: none;">
+                        <path v-if="isDarkMode" d="M 17,0 0,0 0,49.5 17,49.5" stroke="#42d392" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                        <path v-else d="M 17,0 0,0 0,49.5 17,49.5" stroke="#1A1A1A" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                    </svg>
+                    <svg v-else class="absolute -left-4 top-1/2" viewBox="0 0 64 64" height="50" width="50" style="overflow:visible; pointer-events: none;">
+                        <g v-if="isDarkMode">
+                            <path d="M 17,0 0,0 0,6" stroke="#42d392" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                            <circle cx="0" cy="16" fill="#42d392" r="2"/>
+                            <circle cx="0" cy="24" fill="#42d392" r="2"/>
+                            <circle cx="0" cy="32" fill="#42d392" r="2"/>
+                            <path d="M 0,41 0,49.5 17,49.5" stroke="#42d392" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                        </g>
+                        <g v-else>
+                            <path d="M 17,0 0,0 0,6" stroke="#1A1A1A" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                            <circle cx="0" cy="16" fill="#1A1A1A" r="2"/>
+                            <circle cx="0" cy="24" fill="#1A1A1A" r="2"/>
+                            <circle cx="0" cy="32" fill="#1A1A1A" r="2"/>
+                            <path d="M 0,41 0,49.5 17,49.5" stroke="#1A1A1A" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                        </g>
+                    </svg>
+                </div>
+            </Transition>
+        </div>
 
-        <Transition name="fade">
-            <div v-if="isSelected && title"
-                class="text-black dark:text-gray-300 hidden sm:flex -top-12 left-0 place-items-center justify-center absolute z-10 bg-white shadow-xl dark:bg-black-100 text-xs text-left p-2 rounded border-2 border-black dark:border-app-green">
-                {{ title }}
-                <svg v-if="isOpen" class="absolute -left-4 top-1/2" viewBox="0 0 64 64" height="50" width="50" style="overflow:visible; pointer-events: none;">
-                    <path v-if="isDarkMode" d="M 17,0 0,0 0,53.5 17,53.5" stroke="#42d392" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                    <path v-else d="M 17,0 0,0 0,53.5 17,53.5" stroke="#1A1A1A" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                </svg>
-                <svg v-else class="absolute -left-4 top-1/2" viewBox="0 0 64 64" height="50" width="50" style="overflow:visible; pointer-events: none;">
-                    <g v-if="isDarkMode">
-                        <path d="M 17,0 0,0 0,10" stroke="#42d392" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                        <circle cx="0" cy="20" fill="#42d392" r="2"/>
-                        <circle cx="0" cy="28" fill="#42d392" r="2"/>
-                        <circle cx="0" cy="36" fill="#42d392" r="2"/>
-                        <path d="M 0,45 0,53.5 17,53.5" stroke="#42d392" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                    </g>
-                    <g v-else>
-                        <path d="M 17,0 0,0 0,10" stroke="#1A1A1A" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                        <circle cx="0" cy="20" fill="#1A1A1A" r="2"/>
-                        <circle cx="0" cy="28" fill="#1A1A1A" r="2"/>
-                        <circle cx="0" cy="36" fill="#1A1A1A" r="2"/>
-                        <path d="M 0,45 0,53.5 17,53.5" stroke="#1A1A1A" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                    </g>
-                </svg>
-            </div>
-        </Transition>
-    </div>
 </template>
 
 <style scoped>
