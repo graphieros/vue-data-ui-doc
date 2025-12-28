cee412a6bf1572cd5750600136505a96abb9cf50
graphieros <alec.lloyd.probert@gmail.com>
2025-12-28 11:13:17 +0100

    Improve contrinutors chart

--- src/components/ContributorsBars.vue
+++ src/components/ContributorsBars.vue
@@ -33,14 +33,14 @@ const config = computed(() => {
                         gradientIntensity: 0,
                         borderRadius: 1,
                         dataLabels: {
-                            color: isDarkMode.value ? '#CCCCCC' : '#1A1A1A',
+                            color: isDarkMode.value ? '#9A9A9A' : '#4A4A4A',
                             percentage: {
                                 roundingPercentage: 2
                             },
                             offsetX: 12
                         },
                         nameLabels: {
-                            color: isDarkMode.value ? '#CCCCCC' : '#1A1A1A',
+                            color: isDarkMode.value ? '#579ecf' : '#1f77b4',
                             offsetX: -12
                         }
                     },
@@ -48,11 +48,15 @@ const config = computed(() => {
                         color: isDarkMode.value ? '#FFFFFF' : '#1A1A1A'
                     }
                 },
+                title: {
+                    text: 'Commits',
+                    color: isDarkMode.value ? '#579ecf' : '#1f77b4'
+                },
                 tooltip: {
                     borderColor: isDarkMode.value ? '#4A4A4A'  : '#E1E5E8',
                     customFormat: ({ datapoint }) => {
-                        return `<div style="display: flex; flex-direction: column; align-items:center; justify-content:center; padding: 1rem; color:${isDarkMode.value ? '#CCCCCC' : '#1A1A1A'}">
-                            <img height="40" width="40" style="height:40px;width:40px;border-radius:50%;box-shadow: 0 3px 6px - 3px rgba(0,0,0,0.3)" src="${datapoint.avatar}"/>
+                        return `<div style="display: flex; flex-direction: column; align-items:center; justify-content:center; padding: 1rem; color:${isDarkMode.value ? '#CCCCCC' : '#1A1A1A'};border: 2px solid ${isDarkMode.value ? '#3A3A3A' : '#FFFFFF'}">
+                            <img height="40" width="40" style="height:40px;width:40px;border-radius:50%;box-shadow: 0 3px 6px rgba(0,0,0,0.3)" src="${datapoint.avatar}"/>
                             <div>${datapoint.name}</div>
                             <div style="margin-top: 1rem; padding-top: 1rem;border-top: 1px solid ${isDarkMode.value ? '#6A6A6A' : '#CCCCCC'}">
                                 <span>Contributions: ${datapoint.value}</span>    
