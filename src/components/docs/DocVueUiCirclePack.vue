a0f548354d08799db6aa7167e5ebc2b32a123fda
graphieros <alec.lloyd.probert@gmail.com>
2026-05-03 10:10:02 +0200

    3.19.1

--- src/components/docs/DocVueUiCirclePack.vue
+++ src/components/docs/DocVueUiCirclePack.vue
@@ -51,7 +51,55 @@ const isDarkMode = computed(() => {
     return store.isDarkMode;
 });
 
-const dataset = ref(makeDs({ qty: 24, maxVal: 1000 }));
+const dataset = ref([
+    {
+        name: "Datapoint A",
+        value: 200,
+        children: [
+            {
+                name: "Child A-1",
+                value: 100,
+                color: '#d62728',
+                children: [
+                    {
+                        name: 'GrandChild 1',
+                        value: 70,
+                    },
+                    {
+                        name: 'GrandChild 2',
+                        value: 30,
+                    },
+                ]
+            },
+            {
+                name: "Child A-2",
+                value: 100
+            },
+        ]
+    },
+    {
+        name: "Datapoint B",
+        value: 150,
+        children: [
+            {
+                name: "Child B-1",
+                value: 50
+            },
+            {
+                name: "Child B-2",
+                value: 80
+            },
+            {
+                name: "Child B-3",
+                value: 20
+            },
+        ]
+    },
+    {
+        name: "Datapoint C",
+        value: 100
+    },
+]);
 
 const config = ref({
     responsive: false,
@@ -117,8 +165,10 @@ const config = ref({
             outline: "none",
         },
         columnNames: {
-            datapoint: "Datapoint",
-            value: "Value",
+            datapoint: 'Datapoint',
+            parent: 'Parent',
+            depth: 'Depth',
+            value: 'Value',
         },
     },
     style: {
@@ -142,6 +192,24 @@ const config = ref({
                     fontSize: 16,
                     bold: false,
                 },
+            },
+            parentTooltips: {
+                show: true,
+                fontSizeRatio: 1,
+                borderRadiusRatio: 1,
+                color: '#2D353C',
+                filter: 'drop-shadow(0 2px 4px #2D353C30)',
+                backgroundColor: '#F3F4F6',
+                strokeWidth: 1,
+                useSerieColor: true,
+                stroke: '#2D353C',
+                link: {
+                    strokeWidth: 1,
+                    strokeDasharray: 0,
+                    opacity: 0.6,
+                    useSerieColor: false,
+                    stroke: '#2D353C'
+                }
             },
             tooltip: {
                 teleportTo: "body",
@@ -169,6 +237,8 @@ const config = ref({
                     intensity: 40,
                 },
                 labels: {
+                    parents: { show: false },
+                    children: { show: true },
                     name: {
                         fontSizeRatio: 0.8,
                         show: true,
@@ -189,6 +259,10 @@ const config = ref({
                     },
                 },
             },
+            zoom: {
+                show: true,
+                speed: 1
+            }
         },
     },
 });
@@ -258,8 +332,10 @@ const darkModeConfig = ref({
             outline: "none",
         },
         columnNames: {
-            datapoint: "Datapoint",
-            value: "Value",
+            datapoint: 'Datapoint',
+            parent: 'Parent',
+            depth: 'Depth',
+            value: 'Value',
         },
     },
     style: {
@@ -283,6 +359,24 @@ const darkModeConfig = ref({
                     fontSize: 16,
                     bold: false,
                 },
+            },
+            parentTooltips: {
+                show: true,
+                fontSizeRatio: 1,
+                borderRadiusRatio: 1,
+                color: '#CCCCCC',
+                filter: 'drop-shadow(0 2px 4px #00000090)',
+                backgroundColor: '#2A2A2A',
+                strokeWidth: 1,
+                useSerieColor: true,
+                stroke: '#CCCCCC',
+                link: {
+                    strokeWidth: 1,
+                    strokeDasharray: 0,
+                    opacity: 0.6,
+                    useSerieColor: false,
+                    stroke: '#CCCCCC'
+                }
             },
             tooltip: {
                 teleportTo: "body",
@@ -310,6 +404,8 @@ const darkModeConfig = ref({
                     intensity: 40,
                 },
                 labels: {
+                    parents: { show: false },
+                    children: { show: true },
                     name: {
                         fontSizeRatio: 0.8,
                         show: true,
@@ -330,6 +426,10 @@ const darkModeConfig = ref({
                     },
                 },
             },
+            zoom: {
+                show: true,
+                speed: 1
+            }
         },
     },
 });
@@ -401,17 +501,53 @@ const dsTypeCode = computed(() => {
     name: string
     value: number
     color?: string
+    children?: VueUiCirclePackDatasetItem[]
 }`;
 });
 
 const codeDataset = ref(`const dataset: VueUiCirclePackDatasetItem[] = [
     {
         name: "Datapoint A",
-        value: 200
+        value: 200,
+        children: [
+            {
+                name: "Child A-1",
+                value: 100,
+                color: '#d62728',
+                children: [
+                    {
+                        name: 'GrandChild 1',
+                        value: 70,
+                    },
+                    {
+                        name: 'GrandChild 2',
+                        value: 30,
+                    },
+                ]
+            },
+            {
+                name: "Child A-2",
+                value: 100
+            },
+        ]
     },
     {
         name: "Datapoint B",
-        value: 150
+        value: 150,
+        children: [
+            {
+                name: "Child B-1",
+                value: 50
+            },
+            {
+                name: "Child B-2",
+                value: 80
+            },
+            {
+                name: "Child B-3",
+                value: 20
+            },
+        ]
     },
     {
         name: "Datapoint C",
@@ -483,7 +619,7 @@ const customFormatCode =
                 </div>
             </BaseCard>
         </DocSnapper>
-        <BaseRandomButton @click="randomizeData" />
+        <!-- <BaseRandomButton @click="randomizeData" /> -->
     </div>
 
     <Rater itemId="vue_ui_circle_pack" />
@@ -729,6 +865,12 @@ const customFormatCode =
                                     :level="4"
                                     title="style.chart.circles.labels"
                                 >
+                                    <BaseDetails attr="parents" :level="5" title="style.chart.circles.labels.parents">
+                                        <BaseAttr name="show" attr="style.chart.circles.labels.parents.show" type="checkbox" defaultVal="false" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                    </BaseDetails>
+                                    <BaseDetails attr="children" :level="5" title="style.chart.circles.labels.children">
+                                        <BaseAttr name="show" attr="style.chart.circles.labels.children.show" type="checkbox" defaultVal="true" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                    </BaseDetails>
                                     <BaseDetails
                                         attr="name"
                                         :level="5"
@@ -1008,6 +1150,25 @@ const customFormatCode =
                                     />
                                 </BaseDetails>
                             </BaseDetails>
+
+                            <BaseDetails attr="parentTooltips" :level="3" title="style.chart.parentTooltips">
+                                <BaseAttr name="show" attr="style.chart.parentTooltips.show" type="checkbox" defaultVal="true" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                <BaseAttr name="fontSizeRatio" attr="style.chart.parentTooltips.fontSizeRatio" type="number" defaultVal="1" :min="0.5" :max="2" :step="0.1" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                <BaseAttr name="borderRadiusRatio" attr="style.chart.parentTooltips.borderRadiusRatio" type="number" defaultVal="1" :min="0" :max="2" :step="0.1" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                <BaseAttr name="filter" attr="style.chart.parentTooltips.filter" defaultVal="drop-shadow(0 2px 4px #2D353C30)" type="text" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                <BaseAttr name="backgroundColor" attr="style.chart.parentTooltips.backgroundColor" type="color" defaultVal="#FFFFFF" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                <BaseAttr name="strokeWidth" attr="style.chart.parentTooltips.strokeWidth" type="number" defaultVal="1" :min="0" :max="6" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                <BaseAttr name="useSerieColor" attr="style.chart.parentTooltips.useSerieColor" type="checkbox" defaultVal="true" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                <BaseAttr name="stroke" attr="style.chart.parentTooltips.stroke" type="color" defaultVal="#2D353C" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                <BaseDetails attr="link" :level="4" title="style.chart.parentTooltips.link">
+                                    <BaseAttr name="strokeWidth" attr="style.chart.parentTooltips.link.strokeWidth" type="number" defaultVal="1" :min="0" :max="6" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                    <BaseAttr name="strokeDasharray" attr="style.chart.parentTooltips.link.strokeDasharray" type="number" defaultVal="0" :min="0" :max="12" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                    <BaseAttr name="opacity" attr="style.chart.parentTooltips.link.opacity" type="number" defaultVal="0.6" :min="0" :max="1" :step="0.1" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                    <BaseAttr name="useSerieColor" attr="style.chart.parentTooltips.link.useSerieColor" type="checkbox" defaultVal="true" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                    <BaseAttr name="stroke" attr="style.chart.parentTooltips.link.stroke" type="color" defaultVal="#2D353C" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                </BaseDetails>
+                            </BaseDetails>
+
                             <BaseDetails
                                 attr="tooltip"
                                 :level="3"
@@ -1161,6 +1322,11 @@ const customFormatCode =
                                     :dark="mutableConfigDarkMode"
                                 />
                             </BaseDetails>
+
+                            <BaseDetails attr="zoom" :level="3" title="style.chart.zoom">
+                                <BaseAttr name="show" attr="style.chart.zoom.show" type="checkbox" defaultVal="true" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                                <BaseAttr name="speed" attr="style.chart.zoom.speed" type="number" defaultVal="1" :min="0.1" :max="10" :step="0.1" :light="mutableConfig" :dark="mutableConfigDarkMode"/>
+                            </BaseDetails>
                         </BaseDetails>
                     </BaseDetails>
                     <BaseDetails attr="table" :level="1">
@@ -1202,6 +1368,22 @@ const customFormatCode =
                                 attr="table.columnNames.datapoint"
                                 type="text"
                                 defaultVal="Series"
+                                :light="mutableConfig"
+                                :dark="mutableConfigDarkMode"
+                            />
+                            <BaseAttr
+                                name="parent"
+                                attr="table.columnNames.parent"
+                                type="text"
+                                defaultVal="Parent"
+                                :light="mutableConfig"
+                                :dark="mutableConfigDarkMode"
+                            />
+                            <BaseAttr
+                                name="depth"
+                                attr="table.columnNames.depth"
+                                type="text"
+                                defaultVal="Depth"
                                 :light="mutableConfig"
                                 :dark="mutableConfigDarkMode"
                             />
