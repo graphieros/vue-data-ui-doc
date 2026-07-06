37f7085458411b015bc3bcd86aa165c94d9db557
graphieros <alec.lloyd.probert@gmail.com>
2026-07-06 06:40:47 +0200

    Update it dashboard example

--- src/dashboards/DashboardIT.vue
+++ src/dashboards/DashboardIT.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, computed } from "vue";
 import { useMainStore } from "../stores";
+import { VueUiPatternSeed } from "vue-data-ui/vue-ui-pattern-seed";
 
 const store = useMainStore();
 const isDarkMode = computed(() => store.isDarkMode);
@@ -41,6 +42,7 @@ const DATASET_XY_IT_SPENDING = computed(() => {
             series: globalITSpending.value.map((d) => d.spending),
             type: "bar",
             dataLabels: true,
+            color: 'transparent',
             comments: [
                 "",
                 "",
@@ -75,14 +77,13 @@ const CONFIG_XY_IT_SPENDING = computed(() => {
                 show: false,
             },
             height: 400,
-            backgroundColor: isDarkMode.value ? "#4A4A4A" : "#FAFAFA",
-            color: isDarkMode.value ? "#CCCCCC" : "#1A1A1A",
+            backgroundColor: 'transparent',
             grid: {
                 stroke: "transparent",
                 showHorizontalLines: true,
                 labels: {
                     show: false,
-                    color: isDarkMode.value ? "#CCCCCC" : "#1A1A1A",
+                    // color: isDarkMode.value ? "#CCCCCC" : "#1A1A1A",
                     xAxisLabels: {
                         values: globalITSpending.value.map((d) =>
                             String(d.year),
@@ -90,7 +91,7 @@ const CONFIG_XY_IT_SPENDING = computed(() => {
                         fontSize: 12,
                         rotation: -33,
                         yOffset: 0,
-                        color: isDarkMode.value ? "#8A8A8A" : "#1A1A1A",
+                        // color: isDarkMode.value ? "#8A8A8A" : "#1A1A1A",
                     },
                     yAxis: {
                         scaleMax: 6,
@@ -98,7 +99,8 @@ const CONFIG_XY_IT_SPENDING = computed(() => {
                 },
             },
             highlighter: {
-                color: isDarkMode.value ? "#FFFFFF" : "#1A1A1A",
+                opacity: 10,
+                // color: isDarkMode.value ? "#FFFFFF" : "#1A1A1A",
             },
             legend: {
                 show: false,
@@ -111,23 +113,28 @@ const CONFIG_XY_IT_SPENDING = computed(() => {
             },
             title: {
                 text: "Global IT spending",
-                color: isDarkMode.value ? "#549ed1" : "#1f77b4",
+                // color: isDarkMode.value ? "#549ed1" : "#1f77b4",
                 textAlign: "left",
                 paddingLeft: 6,
                 fontSize: 32,
                 subtitle: {
                     text: "2000 - 2025 in Trillion $",
+                    color: '#1A1A1A'
                 },
             },
             tooltip: {
-                color: isDarkMode.value ? "#CCCCCC" : "#1A1A1A",
-                backgroundColor: isDarkMode.value ? "#1A1A1A" : "#FFFFFF",
-                backgroundOpacity: 20,
-                borderColor: isDarkMode.value ? "#6A6A6A" : "#e1e5e8",
+                // color: isDarkMode.value ? "#CCCCCC" : "#1A1A1A",
+                backgroundColor: 'transparent',
+                backgroundOpacity: 0,
                 showPercentage: false,
                 roundingValue: 2,
+                borderColor: 'transparent',
+                backdropFilter: false,
+                teleportTo: "#it-dash"
+
             },
             zoom: {
+                show: false,
                 color: isDarkMode.value ? "#8A8A8A" : "#CCCCCC",
                 highlightColor: isDarkMode.value ? "#1f77b4" : "#1f77b4",
                 minimap: {
@@ -145,17 +152,73 @@ const CONFIG_XY_IT_SPENDING = computed(() => {
             labels: {
                 show: true,
                 rounding: 2,
-                color: isDarkMode.value ? "#549ed1" : "#1f77b4",
+                // color: isDarkMode.value ? "#549ed1" : "#1f77b4",
             },
         },
     };
 });
+
 </script>
 
 <template>
     <div
-        class="flex flex-row flex-wrap max-w-[1200px] mx-auto gap-4 mt-12 p-4 bg-white dark:bg-[#3A3A3A]"
+        id="it-dash"
+        class="flex flex-row flex-wrap max-w-[1200px] mx-auto gap-4 mt-12 p-4 bg-white dark:bg-[#3A3A3A] font-inter-bold"
     >
+        <div class="flex w-full mx-auto px-4" id="it-chart">
+        <div
+            class="w-full p-2 rounded shadow-md font-inter-bold"
+        >
+            <VueDataUi
+                component="VueUiXy"
+                :dataset="DATASET_XY_IT_SPENDING"
+                :config="CONFIG_XY_IT_SPENDING"
+            >
+                <template #tooltip="{ datapoint, timeLabel }">
+                    <div class="paper-bg font-inter-bold p-4 flex flex-col rounded min-w-[100px]">
+                        <span>
+                            {{ timeLabel.text }}
+                        </span>
+                        <span class="text-2xl">
+                            {{ datapoint[0].value }}
+                        </span>
+                    </div>
+                </template>
+
+                <template #pattern="{ seriesIndex, patternId }">
+                    <VueUiPatternSeed
+                        :id="patternId"
+                        :seed="seriesIndex"
+                        foreground-color="#1A1A1A"
+                        background-color="transparent"
+                        :max-size="2"
+                        :min-size="12"
+                        :disambiguator="0.58"
+                    />
+                </template>
+
+                <template #plot-comment="{ plot }">
+                    <div
+                        :style="{
+                            width: '100%',
+                            textAlign: 'center',
+                            marginTop: '-80px',
+                        }"
+                        class="flex flex-col place-items-center text-xs gap-2"
+                    >
+                        {{ plot.comment }}
+                        <div class="animate-ping">
+                            <VueUiIcon
+                                name="circleExclamation"
+                                :size="16"
+                                :stroke="isDarkMode ? '#ff6600' : '#ff6600'"
+                            />
+                        </div>
+                    </div>
+                </template>
+            </VueDataUi>
+        </div>
+    </div>
         <div
             class="flex-1 min-w-[300px] p-4 bg-[#FAFAFA] dark:bg-[#4A4A4A] rounded shadow-md"
         >
@@ -195,7 +258,7 @@ const CONFIG_XY_IT_SPENDING = computed(() => {
                             },
                         }"
                     />
-                    <div class="text-xs mt-3">
+                    <div class="text-xs mt-3 font-inter-medium">
                         of organizations plan to increase their IT budgets in
                         2025. Total IT spending is expected to grow 9% year over
                         year.
@@ -243,7 +306,7 @@ const CONFIG_XY_IT_SPENDING = computed(() => {
                             },
                         }"
                     />
-                    <div class="text-xs mt-3">
+                    <div class="text-xs mt-3 font-inter-medium">
                         of companies plan to increase spending on generative AI,
                         and 52% plan to raise security budgets in 2025.
                     </div>
@@ -290,41 +353,9 @@ const CONFIG_XY_IT_SPENDING = computed(() => {
                             },
                         }"
                     />
-                    <div class="text-xs mt-3">
+                    <div class="text-xs mt-3 font-inter-medium">
                         of senior tech leaders believe hiring IT talent is
                         difficult amid skills gaps.
-                    </div>
-                </template>
-            </VueDataUi>
-        </div>
-    </div>
-
-    <div class="flex max-w-[1200px] mx-auto px-4 bg-white dark:bg-[#3A3A3A]">
-        <div
-            class="w-full p-2 bg-[#FAFAFA] dark:bg-[#4A4A4A] rounded shadow-md"
-        >
-            <VueDataUi
-                component="VueUiXy"
-                :dataset="DATASET_XY_IT_SPENDING"
-                :config="CONFIG_XY_IT_SPENDING"
-            >
-                <template #plot-comment="{ plot }">
-                    <div
-                        :style="{
-                            width: '100%',
-                            textAlign: 'center',
-                            marginTop: '-80px',
-                        }"
-                        class="flex flex-col place-items-center text-xs gap-2"
-                    >
-                        {{ plot.comment }}
-                        <div class="animate-ping">
-                            <VueUiIcon
-                                name="circleExclamation"
-                                :size="16"
-                                :stroke="isDarkMode ? '#ff6600' : '#ff6600'"
-                            />
-                        </div>
                     </div>
                 </template>
             </VueDataUi>
@@ -334,3 +365,20 @@ const CONFIG_XY_IT_SPENDING = computed(() => {
         class="grid grid-cols-1 sm:grid-cols-2 max-w-[1200px] mx-auto gap-4 p-4 bg-white dark:bg-[#3A3A3A]"
     ></div>
 </template>
+
+<style scoped>
+#it-chart,
+.paper-bg {
+    background-image: url("../assets/paper.jpg");
+    background-size: cover;
+}
+</style>
+
+<style>
+#it-dash .vue-data-ui-tooltip {
+    box-shadow: none !important;
+}
+#it-dash .paper-bg {
+    box-shadow: 0 12px 24px -12px black;
+}
+</style>
